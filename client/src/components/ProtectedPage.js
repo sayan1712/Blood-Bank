8cc5e0af295972628985310aa12297280f2837db
099ayuh <hiayushsingh.co.in@gmail.com>
2023-10-27 19:16:43 +0530

    bug fixed

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -1,49 +1,50 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { message } from 'antd';
 import { GetCurrentUser } from '../apicalls/users';
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { getLoggedInUserName } from '../utils/helpers';
 import { useDispatch, useSelector } from 'react-redux';
 import { SetCurrentUser } from '../redux/userSlice';
 
 
 
-export default function ProtectedPage({children}) {
-    const {currentUser} = useSelector((state)=>state.users);
+export default function ProtectedPage({ children }) {
+    const { currentUser } = useSelector((state) => state.users);
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const getCurrentUser=async() =>{
+    const getCurrentUser = async () => {
         try {
-            const response=await GetCurrentUser();
-            if (response.sccess){
+            const response = await GetCurrentUser();
+            if (response.success) {
                 message.success(response.message);
                 dispatch(SetCurrentUser(response.data))
             }
-            else{
+            else {
                 throw new Error(response.message);
             }
 
         }
-        catch (error){
+        catch (error) {
+            console.log(error.message)
             message.error(error.message);
         }
     };
 
     useEffect(() => {
-        if(localStorage.getItem("token")){
+        if (localStorage.getItem("token")) {
             getCurrentUser();
-        } else{
+        } else {
             navigate("/login");
         }
-    },[]);
+    }, []);
 
-    return(
+    return (
         currentUser && (<div>
             <h1>Welcome {getLoggedInUserName(currentUser.name)}</h1>
-       
-           {children}</div>)
-    
+
+            {children}</div>)
+
     )
-    
+
 }
